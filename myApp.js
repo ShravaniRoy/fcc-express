62833c6cb0b9b84f9a1713a657376c2f07c4f016
ShravaniRoy <shravani.thirunagari@gmail.com>
2023-08-06 10:40:04 +0530

    serving simple json

--- myApp.js
+++ myApp.js
@@ -8,9 +8,14 @@ let app = express();
 
 app.use('/public', express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
+app.get('/', function(req, res){ //serving a file
     absolutePath = __dirname + '/views/index.html'
     res.sendFile(absolutePath);
+})
+
+app.get('/json', function(req, res){ //serving a JSON object
+    
+    res.send({"message": "Hello json"});
 })
 
 
